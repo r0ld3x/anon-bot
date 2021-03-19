1553dacb47c2d9a7faf3495b55880e8bd95d6175
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-03-19 16:41:01 +0530

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -4,6 +4,6 @@ TEXT="""
 This Feature Not Available Yet😔. \n Tell Me If You Want This At @arkcodes
 """
 @Client.on_message(filters.group & ~filters.channel)
-async def leave(client, message)
+async def leave(client, message):
 await message.reply_text(TEXT)
 await message.chat.leave()
