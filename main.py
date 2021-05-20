1227d180617d4066b9a4d26f7ed02dbfb689cda0
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-05-20 08:25:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 roldex = Client('anon-bot',
-                  api_id=var.API_ID,
-                  api_hash=var.API_HASH,
-                  bot_token=var.BOT_TOKEN,                
+                  api_id="1667849,
+                  api_hash="b719710209932bff18219f4064e92388",
+                  bot_token="1771171629:AAGhmkT9XMzt6oLnLugzJm5tZKErNM-FnhY",                
                   plugins=dict(root="plugins"))                            
 roldex.run()
