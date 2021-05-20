9f434ed6f897719d0e7c7e9a954b3bf01a7b7797
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-05-20 08:44:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,8 +5,8 @@ load_dotenv(find_dotenv())
 
 
 class var:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
-    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
-    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
-    START_MESSAGE = os.getenv("START_MESSAGE", None)  # Not Mandatory
-    CAPTION_MESSAGE = os.getenv("CUSTOM_CAPTION") #custom caption For Caption.py
+    BOT_TOKEN = "1771171629:AAGhmkT9XMzt6oLnLugzJm5tZKErNM-FnhY"  # from @botfather
+    API_ID = "1667849" # from https://my.telegram.org/apps
+    API_HASH =  "b719710209932bff18219f4064e92388" # from https://my.telegram.org/apps
+    START_MESSAGE = "" # Not Mandatory
+    CAPTION_MESSAGE = "" #custom caption For Caption.py
